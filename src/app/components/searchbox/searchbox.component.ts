bc1cd55ea6916e8ee981aff6d699b0283355acfe
yoth <130324062+yoth-hi@users.noreply.github.com>
2024-08-04 04:05:59 +0000

    0.0.0.0.1

---
+++ src/app/components/searchbox/searchbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './searchbox.component.html',
+  styleUrl: './searchbox.component.scss'
+})
+export class SearchboxComponent {
+
+}
