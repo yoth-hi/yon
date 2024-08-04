9482ab432d62b723f468d9eaeaed934c4395da45
yoth <130324062+yoth-hi@users.noreply.github.com>
2024-08-04 04:17:24 +0000

    0.0.0.0.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './pages/feed/feed.component';
+import { WatchComponent } from './pages/watch/watch.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { component: FeedComponent, path: "" },
+    { component: WatchComponent, path: "watch" },
+];
